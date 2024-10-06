e4ed5bee750dcc0da45a210cd8fd22b62bcab5a7
sancristm <sancristma@gmail.com>
2024-10-07 01:29:38 +0300

    created RegistrationController

---
+++ src/main/java/com/example/Blogpost/Application/Registration/RegistrationController.java
@@ -0,0 +1,26 @@
+package com.example.Blogpost.Application.Registration;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping( path="api/v1/registration")
+@AllArgsConstructor
+
+
+public class RegistrationController {
+
+
+    private final  RegistrationService registrationService;
+
+    public String register(@RequestBody RegistrationRequest request){
+
+        return  RegistrationService.register(request);
+    }
+
+
+
+}
