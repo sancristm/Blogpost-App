5ce8702b0c9cb3716837cbd5490bfda33e973947
sancristm <sancristma@gmail.com>
2024-10-07 01:30:40 +0300

    requests registration from the client handling

---
+++ src/main/java/com/example/Blogpost/Application/Registration/RegistrationRequest.java
@@ -0,0 +1,26 @@
+package com.example.Blogpost.Application.Registration;
+
+
+
+
+
+//WHEN THE clients sends the request capture details
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class RegistrationRequest {
+
+    private final String firstName;
+    private final String lastName;
+    private final String password;
+    private final String email;
+
+
+
+
+}
