46af2a1928fc52012f6c37135c1cb34c71f48789
sancristm <sancristma@gmail.com>
2024-10-07 01:24:57 +0300

    created user repository

---
+++ src/main/java/com/example/Blogpost/Application/Appuser/AppUserRepository.java
@@ -0,0 +1,18 @@
+package com.example.Blogpost.Application.Appuser;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface AppUserRepository extends JpaRepository<AppUser, Long > {
+
+    Optional<AppUser> findByEmail ( String email);
+
+
+}
