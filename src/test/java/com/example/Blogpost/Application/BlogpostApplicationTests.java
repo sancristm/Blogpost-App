6771e335ac192bf0c5d6482987725c75b8dbaf7e
sancristm <sancristma@gmail.com>
2024-10-06 13:11:52 +0300

    Initialization

---
+++ src/test/java/com/example/Blogpost/Application/BlogpostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Blogpost.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
